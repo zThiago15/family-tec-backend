e071812ccab0199ef0b12eb590d334b1e76dad52
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-08 00:33:21 -0300

    test(unit): feedbackController and its getAll, getTopFive and create methods

---
+++ src/tests/mocks/feedback.ts
@@ -0,0 +1,46 @@
+const allFeedbacksMock = [{
+    id: 1,
+    name: 'John',
+    feedback: 'the service was great!',
+    starRating: 5
+}]
+
+export const feedbackToCreateMock = {
+    name: 'John',
+    feedback: 'the service was great!',
+    starRating: 5
+}
+
+export const topFiveFeedbacksMock = [{
+    id: 1,
+    name: 'John',
+    feedback: 'the service was great!',
+    starRating: 5
+},
+{
+    id: 2,
+    name: 'Maria',
+    feedback: 'the service was great!',
+    starRating: 5
+},
+{
+    id: 3,
+    name: 'João',
+    feedback: 'the service was great!',
+    starRating: 5
+},
+{
+    id: 4,
+    name: 'Gisele',
+    feedback: 'the service was great!',
+    starRating: 5
+},
+{
+    id: 5,
+    name: 'Maria',
+    feedback: 'the service was great!',
+    starRating: 5
+}
+]
+
+export default allFeedbacksMock;
---
+++ src/tests/services/feedbackService.spec.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client"
+import { expect } from "chai";
+import sinon from 'sinon';
+import FeedbackService from "../../services/feedbacksService";
+import allFeedbacksMock, { feedbackToCreateMock, topFiveFeedbacksMock } from "../mocks/feedback";
+
+describe('feedbackService', () => {
+    const feedbackService = new FeedbackService();
+
+    beforeEach(() => {
+        const prisma = new PrismaClient()
+        
+        sinon.stub(prisma.feedback , 'create').resolves(allFeedbacksMock[0]);
+        sinon.stub(feedbackService, 'getAll').resolves(allFeedbacksMock);
+        sinon.stub(feedbackService, 'getTopFive').resolves(topFiveFeedbacksMock);
+    })
+
+    afterEach(() => {
+        sinon.restore()
+    })
+
+    describe('create', () => {
+        it('should return a true value after creating feedback', async () => {
+            const response = await feedbackService.create(feedbackToCreateMock);
+
+            expect(response).to.be.true;
+        })
+    })
+
+    describe('getAll', () => {
+        it('should return all the feedbacks', async () => {
+            const response = await feedbackService.getAll();
+
+            expect(response).to.be.equal(allFeedbacksMock);
+        })
+    })
+
+    describe('create', () => {
+        it('should return top five feedbacks', async () => {
+            const response = await feedbackService.getTopFive();
+
+            expect(response).to.be.equal(topFiveFeedbacksMock);
+        })
+    })
+})
