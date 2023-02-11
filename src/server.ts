60902538b7b6446e1a75d4b58eb166791664ff73
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-11 01:24:11 -0300

    feat: add welcome router at GET '/'

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, Router } from 'express';
 import feedbackRouter from './routes/feedback';
 import 'express-async-errors'
 import cors from 'cors';
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use(feedbackRouter)
 
 app.use(errorMiddleware);
+
+app.get('/', (_req: Request, res: Response) => {
+    return res.status(200).send('Welcome to server')
+});
 
 const port = process.env.PORT || 3000;
 
