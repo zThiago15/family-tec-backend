d686c6d3a08ffad0ad49c23253790faddb2fd2ea
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-11 02:03:02 -0300

    docs: configuration of api documementation

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import feedbackRouter from './routes/feedback';
 import 'express-async-errors'
 import cors from 'cors';
 import errorMiddleware from './errorHandling/errorMiddleware';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './docs/swagger.json';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(feedbackRouter)
 
 app.use(errorMiddleware);
@@ -15,6 +18,7 @@ app.use(errorMiddleware);
 app.get('/', (_req: Request, res: Response) => {
     return res.status(200).send('Welcome to server')
 });
+
 
 const port = process.env.PORT || 3000;
 
